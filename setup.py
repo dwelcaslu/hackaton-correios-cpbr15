3deaeba01daa147a3583fa14cbf64d3eb1d43ebd
Weld Lucas Cunhas <cunha.lucas012@gmail.com>
2023-07-27 23:21:26 -0300

    repo initial folders structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='O Hackathon que será realizado na CPBR15, trará o tema: “Como podemos universalizar a codificação postal em nível de logradouro para realizar novos projetos com maior agilidade, assertividade e menor dependência de insumos de órgãos externos aos Correios?”. O objetivo é promover o desenvolvimento de soluções inovadoras que utilizem a tecnologia para encontrar soluções para a questão proposta.',
+    author='AWP',
+    license='MIT',
+)
